d543a6ab1e0a572d53086ee498bdc470af8503eb
Jason_song97 <107292103+JasonSong97@users.noreply.github.com>
2023-01-03 22:16:36 +0900

    프로그래머스 레벨 1

---
+++ pg_12940/PG_12940.java
@@ -0,0 +1,44 @@
+package src.pg.pg_12940;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class PG_12940 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        int[] answer = new int[2];
+
+        if (n < m) {// m 작은값 만드는 과정 < n 큰값
+            int temp = m;
+            m = n;
+            n = temp;
+        }
+
+
+        answer[0] = gcd(n, m);
+        answer[1] = n * m / answer[0];
+
+        System.out.println(Arrays.toString(answer));
+    }
+
+    static int gcd(int n, int m) {
+        int k;
+        while (m > 0) {// (648, 232) -> (232, 184) -> (184, 48) -> (48, 40) -> (40, 8)!
+            k = n % m;
+            n = m;
+            m = k;
+        }
+        return n;
+    }
+    /*
+    public static int gcd(int p, int q) {
+        if (q == 0) return p;
+        return gcd(q, p % q);
+    }
+
+
+
+     */
+}
